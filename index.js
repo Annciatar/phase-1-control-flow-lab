eb7a796950693cf63f7b9697bf75324ed230e5dc
Annciatar Ndumi <annciatar.kavita@student.moringaschool.com>
2022-08-12 14:11:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feeRide){
   // Write your code here!
+if (feeRide <= 400){
+  return 'This one is on me!';
+} else if(feeRide > 2000 && feeRide <= 2500){
+  return 'I will gladly take your thirty bucks.';
+}else if( feeRide > 2500){
+  return 'No can do.'
+}
+}
+  
+
+
+function ternaryCheckCity(place){
+  // Write your code here!
+  return place === 'NYC'? 'Ok, sounds good.' : 'No go.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(thanks){
   // Write your code here!
+switch(thanks){
+  case 'generous':
+    return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+      case 'thanks for everything':
+        return 'Bye.'
+}
 }
